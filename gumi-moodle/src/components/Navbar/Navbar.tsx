024544f87f4f7935e77178a0e7ce150a7786d0a1
Mexolius <39342674+Mexolius@users.noreply.github.com>
2021-03-27 17:34:37 +0100

    GMS-44: Strona logowanie

--- gumi-moodle/src/components/Navbar/Navbar.tsx
+++ gumi-moodle/src/components/Navbar/Navbar.tsx
@@ -3,6 +3,7 @@ import 'w3-css/w3.css';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Home from '../Home/Home';
 import Profile from '../Profile/Profile';
+import Login from '../Login/Login';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 
@@ -15,11 +16,13 @@ class Navbar extends Component {
                         <div className="w3-right">
                             <Link to={'/'} className="w3-bar-item w3-button"><FontAwesomeIcon icon="check-square" /> Home</Link>
                             <Link to={'/profile'} className="w3-bar-item w3-button">Profile</Link>
+                            <Link to={'/login'} className="w3-bar-item w3-button">Login</Link>
                         </div>  
                     </div>
                 <Switch>
                         <Route exact path='/' component={Home} />
                         <Route path='/profile' component={Profile} />
+                        <Route path='/login' component={Login} />
                 </Switch>
             </Router>
         );
